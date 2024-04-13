4e461e1f2c7b8ceb9fea63ab8d609a454da51e2f
Abid Dar <abiddar1979@gmail.com>
2024-04-13 14:59:10 +0500

    files added

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+/* Task # 13 : Your Own Array : Think of your favourite mode of Transportation, such as a motorcycle or a car, and make a list that stores severel examples. Use your list to print a series of statements about these items, such as "I would like to own a Honda Motorcycle".*/
+let transportation = ["Harley Davidson", "Sports car", "Tiatanic Ship", "Aeroplane"];
+console.log(`"I would like to own a" : ${transportation[0]}`);
+console.log(`"I would like to own a" : ${transportation[1]}`);
+console.log(`"I would like to own a" : ${transportation[2]}`);
+console.log(`"I would like to own a" : ${transportation[3]}`);
---
+++ app.ts
@@ -0,0 +1,10 @@
+/* Task # 13 : Your Own Array : Think of your favourite mode of Transportation, such as a motorcycle or a car, and make a list that stores severel examples. Use your list to print a series of statements about these items, such as "I would like to own a Honda Motorcycle".*/
+
+
+let transportation: string[] = ["Harley Davidson", "Sports car", "Tiatanic Ship", "Aeroplane"];
+
+
+console.log(`"I would like to own a" : ${transportation[0]}`);
+console.log(`"I would like to own a" : ${transportation[1]}`);
+console.log(`"I would like to own a" : ${transportation[2]}`);
+console.log(`"I would like to own a" : ${transportation[3]}`);
